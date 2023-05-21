fbd5b08c74f991fc0022998ffba7283884fa344b
ZhmakaRoman <127633639+ZhmakaRoman@users.noreply.github.com>
2023-05-21 11:03:46 +0200

    Update Skull.cs

--- Assets/Scripts/Skull.cs
+++ Assets/Scripts/Skull.cs
@@ -4,17 +4,5 @@ using UnityEngine;
 
 public class Skull : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _skullPrefab;
-    [SerializeField]
-    private Transform _spawnSkull;
-
-    public IEnumerator CallTheSkull()
-    {
-        yield return new WaitForSeconds(2);
-        if (_skullPrefab != null)
-        {
-            _spawnSkull.transform.position = _spawnSkull.position;
-        }
-    }
+   
 }
